9562fc1f29a1d0eddcdb1dc5381898c7c2b337aa
Frederic Soumare <frederic.soumare@gmail.com>
2024-08-26 21:37:27 +0200

    Modify the raytracer to render a scene
    Modify the raytracing main to render a scene using everything built so
    far : a world struct, spheres, transforms, a camera etc.

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -5,6 +5,9 @@
 #include "maths.h"
 #include "material.h"
 #include "light.h"
+#include "world.h"
+#include "transform.h"
+#include "camera.h"
 
 
 void render2D()
@@ -103,7 +106,73 @@ void render3D()
   std::cout << c.canvas_to_ppm();
 }
 
+void renderScence()
+{
+  World world{};
+
+  // The floor is an extremely flattened sphere with a matte texture
+  Sphere floor{};
+  floor.transform = scaling(10, 0.01, 10);
+  floor.material.color = Color{1, 0.9, 0.9};
+  floor.material.specular = 0;
+
+  // The wall on the left has the same scale and color as teh floor, but is
+  // also rotated and translated into place
+  // Note: the transformations are multiplied in the reverse order
+  Sphere left_wall{};
+  left_wall.transform = translation(0, 0, 5) * rotation_y(-pi/4) *
+    rotation_x(pi/2) * scaling(10, 0.01, 10);
+  left_wall.material = floor.material;
+
+  // The wall on the right is identical to the left wall, but is rotated the
+  // opposite direction in y
+  Sphere right_wall{};
+  right_wall.transform  = translation(0, 0, 5) * rotation_y(pi/4) *
+    rotation_x(pi/2) * scaling(10, 0.01, 10);
+  right_wall.material = floor.material;
+
+  // The large sphere in the middle is a unit sphere, translated upward
+  // slightly and colored green.
+  Sphere middle{};
+  middle.transform = translation(-0.5, 1, 0.5);
+  middle.material.color = Color{0.1, 1, 0.5};
+  middle.material.diffuse = 0.7;
+  middle.material.specular = 0.3;
+
+  // The smaller green sphere on the right is scaled in half
+  Sphere right{};
+  right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5);
+  right.material.color = Color{0.5, 1, 0.1};
+  right.material.diffuse = 0.7;
+  right.material.specular = 0.3;
+
+  // The smallest sphere is scaled by a third, before being translated
+  Sphere left{};
+  left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33);
+  left.material.color = Color{1, 0.8, 0.1};
+  left.material.diffuse = 0.7;
+  left.material.specular = 0.3;
+
+  world.objects.push_back(floor);
+  world.objects.push_back(left_wall);
+  world.objects.push_back(right_wall);
+  world.objects.push_back(middle);
+  world.objects.push_back(right);
+  world.objects.push_back(left);
+
+  // The light source is white, shining from above and to the left
+  world.light = PointLight{Point{-10, 10, -10}, Color{1, 1, 1}};
+
+  // Camera camera{100, 50, pi/3};
+  Camera camera{1000, 500, pi/3};
+  camera.transform = view_transform(Point{0, 1.5, -5}, Point{0, 1, 0}, Vector{0, 1, 0});
+
+  Canvas canvas = render(camera, world);
+
+  std::cout << canvas.canvas_to_ppm();
+}
+
 int main()
 {
-  render3D();
+  renderScence();
 }
